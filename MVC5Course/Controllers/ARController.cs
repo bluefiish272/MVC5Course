c0ca13fdcd9ad82a740ce87be48155799d1ce398
Achito <bluefish272@gmail.com>
2016-10-29 14:38:12 +0800

    42 練習從 Action 傳資料到 View 裡 (ViewData, ViewBag, TempData)

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -6,12 +6,50 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class ARController : Controller
+    public class ARController : BaseController
     {
+
         // GET: AR
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViewTest()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ComtentTest()
+        {
+            return Content("MVC5課程", "text/plain");
+        }
+
+        public ActionResult FileTest()
+        {
+            //C:\Users\user\Source\Repos\MVC5Course\MVC5Course\Content\Lucky.jpg
+            var filepath = Server.MapPath("~/Content/Lucky.jpg");
+            return File(filepath,"img/jpeg");
+        }
+
+        public ActionResult FileTest2()
+        {
+            //C:\Users\user\Source\Repos\MVC5Course\MVC5Course\Content\Lucky.jpg
+            var filepath = Server.MapPath("~/Content/Lucky.jpg");
+            return File(filepath, "img/jpeg","ShenYen.jpg");
+        }
+
+        public ActionResult JsonTest()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            //延遲載入，避免循環參考
+            var data = db.Product.OrderByDescending(p => p.ProductId).Take(10);
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,51 @@
+﻿using MVC5Course.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            ViewData["Temp1"] = "暫存資料1";
+
+            var o = new ClientLoginViewModel()
+            {
+                FirstName = "Lucky",
+                LastName = "Yen"
+            };
+
+            ViewData["Temp2"] = o;
+
+            ViewBag.Temp3 = o;
+
+            return View();
+        }
+
+        public ActionResult MyForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MyForm(ClientLoginViewModel c)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["MyFormData"] = c;
+                return Redirect("MyFormResult");
+            }
+            return View();
+        }
+
+        public ActionResult MyFormResult()
+        {
+            return View();
+        }
+    }
+}
